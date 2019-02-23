a271bef3fdeda77999f435f575807a7bf537f1b7
Rich Smith <rich@richjdsmith.ca>
2019-02-23 12:36:32 +0000

    Updated Generate QuizData to generate one array of all headlines

--- lambda/custom/generateQuizData.js
+++ lambda/custom/generateQuizData.js
@@ -1,17 +1,22 @@
 // https://old.reddit.com/r/nottheonion/top/.json?sort=top&t=month&limit=100
 var fs = require('fs');
 const axios = require('axios');
-let theOnionData = [];
-let notTheOnionData = [];
+let headlinesArray = [];
+let theOnionStorageArray = [];
+let notTheOnionStorageArray = [];
 
 async function fetchData(storageArray, subreddit) {
   console.log('in fetchData');
   let pagination = '';
+
   await axios.get(`https://old.reddit.com/r/${subreddit}/top/.json?sort=top&t=month&limit=100&after=${pagination}`)
     .then(result => {
       let posts = result.data.data.children;
       posts.forEach(post => {
-        storageArray.push(post.data.title);
+        let postObject = {};
+        postObject.category = subreddit;
+        postObject.headline = post.data.title;
+        storageArray.push(postObject);
       });
       pagination = result.data.data.after;
       return pagination;
@@ -20,7 +25,10 @@ async function fetchData(storageArray, subreddit) {
     .then(result => {
       let posts = result.data.data.children;
       posts.forEach(post => {
-        storageArray.push(post.data.title);
+        let postObject = {};
+        postObject.category = subreddit;
+        postObject.headline = post.data.title;
+        storageArray.push(postObject);
       });
       pagination = result.data.data.after;
       return pagination;
@@ -29,7 +37,10 @@ async function fetchData(storageArray, subreddit) {
     .then(result => {
       let posts = result.data.data.children;
       posts.forEach(post => {
-        storageArray.push(post.data.title);
+        let postObject = {};
+        postObject.category = subreddit;
+        postObject.headline = post.data.title;
+        storageArray.push(postObject);
       });
       pagination = result.data.data.after;
       return storageArray;
@@ -37,13 +48,18 @@ async function fetchData(storageArray, subreddit) {
   return storageArray;
 }
 
+// async function consolidateArrays(theOnion, notTheOnion) {
+//   console.log('in consolidateArrays');
+//   let object = {};
+//   object.notTheOnionHeadlines = notTheOnion;
+//   object.theOnionHeadlines = theOnion;
+//   console.log(object);
+//   return object;
+// }
 async function consolidateArrays(theOnion, notTheOnion) {
   console.log('in consolidateArrays');
-  let object = {};
-  object.notTheOnionHeadlines = notTheOnion;
-  object.theOnionHeadlines = theOnion;
-  console.log(object);
-  return object;
+  const consolidateArray = [...notTheOnion, ...theOnion];
+  return consolidateArray;
 }
 
 async function writeArrays(object) {
@@ -61,12 +77,12 @@ async function writeArrays(object) {
 
 // The one that does the thing - that is, this function is responsible for calling all the other functions. 
 async function getData() {
-  const theOnionArrayPromise = fetchData(theOnionData, 'theonion');
-  const notTheOnionArrayPromise = fetchData(notTheOnionData, 'nottheonion');
+  const theOnionArrayPromise = fetchData(theOnionStorageArray, 'theonion');
+  const notTheOnionArrayPromise = fetchData(notTheOnionStorageArray, 'nottheonion');
   const theOnionArray = await theOnionArrayPromise;
   const notTheOnionArray = await notTheOnionArrayPromise;
-  const consolidatedArrays = await consolidateArrays(theOnionArray, notTheOnionArray);
-  await writeArrays(consolidatedArrays);
+  const consolidatedArray = await consolidateArrays(theOnionArray, notTheOnionArray);
+  await writeArrays(consolidatedArray);
 }
 
 getData();
